4a612c074aa75aad7568e6a02ec9215bfb876121
LGedas <g.liaudanskis@gmail.com>
2023-09-28 20:34:46 +0300

    reverseCase method completed

--- EdabitTasks/ReverseCase.cs
+++ EdabitTasks/ReverseCase.cs
@@ -1,10 +1,29 @@
-﻿namespace EdabitTasks
+﻿using System;
+
+namespace EdabitTasks
 {
     public class ReverseCase
     {
         public static string reverseCase(string letters) 
         {
             string revertedLetters = string.Empty;
+
+            foreach (char letter in letters) 
+            {
+                if (char.IsUpper(letter))
+                {
+                    revertedLetters += char.ToLower(letter);
+                    continue;
+                }
+
+                if (char.IsLower(letter))
+                {
+                    revertedLetters += char.ToUpper(letter);
+                    continue;
+                }
+
+                revertedLetters += letter;
+            }
 
             return revertedLetters;
         }
